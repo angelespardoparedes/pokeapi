a3512ae10fc814022c7bcef14929bb11c139a77f
angelespardoparedes <angeles.pardopared@gmail.com>
2024-02-17 11:23:12 +0100

    mejorando el poner color según tipo de pokemon

--- pokedex.js
+++ pokedex.js
@@ -6,11 +6,13 @@ async function fetchPokemons() {
     for (let i = 1; i <= 150; i++) {
         const response = await fetch(`${baseUrl}${i}`);
         const pokemon = await response.json();
+        const primaryType = pokemon.types[0].type.name; // Obtener solo el primer tipo
         
         const pokemonInfo = {
             name: pokemon.name,
             image: pokemon.sprites['front_default'],
             type: pokemon.types.map(type => type.type.name).join(', '),
+            primaryType: primaryType, //cojo solo el primero para usar el color correspondiente a este más tarde
             id: pokemon.id
         };
 
@@ -24,9 +26,9 @@ function renderPokemon(pokemon) {
     const pokemonElement = document.createElement('div');
     pokemonElement.classList.add('pokemon');
 
-    // Eliminar espacios en blanco y convertir a minúsculas antes de agregar la clase del tipo pokemon, esto lo hago para poder poner color segun el tipo de pokemon
-    const typeClass = pokemon.type.toLowerCase().replace(' ', '');
-    pokemonElement.classList.add(`type-${typeClass}`);
+    // Eliminar espacios en blanco y convertir a minúsculas antes de agregar la clase del tipo pokemon esto lo hago para poder poner color segun el tipo de pokemon
+    const typeClass = pokemon.primaryType.toLowerCase().replace(' ', '');
+    pokemonElement.classList.add(`type-${typeClass}`); // Saco clase a partir del tipo de pokemon
 
     const pokemonInnerHTML = `
         <img src="${pokemon.image}" alt="${pokemon.name}">
